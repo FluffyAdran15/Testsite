cc03ffeb58f6473129396cbe7ef2c7d29d8629d5
Adrian <asoliz241@west-mec.org>
2021-04-28 17:12:34 -0700

    Fixed and merge a my Bars.js and fixed my index page

--- Bar.js
+++ Bar.js
@@ -1,6 +1,9 @@
 var i = 0;
-
+var j = 0
+var k = 0
 window.onload = Java()
+window.onload = html();
+window.onload = move()
 
 
 function Java() {
@@ -23,12 +26,57 @@ function Java() {
 }
 
 
-window.onscroll = function() {myFunction()};
+
+function html() {
+  if (j == 0) {
+    j = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 15);
+    function frame() {
+      if (width >= 95) {
+        clearInterval(id);
+        j = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width  + "%";
+      }
+    }
+  }
+}
+
+
+function move() {
+  if (k == 0) {
+    k = 1;
+    var elem = document.getElementById("Bar");
+    var width = 0;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 90) {
+        clearInterval(id);
+        k = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width  + "%";
+      }
+    }
+  }
+}
+
+
+
+
+
+
+window.onscroll = function() {Nav()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function Nav() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
